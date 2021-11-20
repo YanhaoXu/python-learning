83430d0be339573422798ad9db03e90b58a7b5a6
yanhao.xu <yanhao.xu@outlook.com>
2021-11-20 23:36:37 +0800

    feature(ch02): chapter 2 elementary programming
    source add

---
+++ pybook/ch03/DisplayUnicode.py
@@ -0,0 +1,5 @@
+import turtle
+
+turtle.write("\u6B22\u8FEC \u03b1 \u03b2 \u03b3")
+
+turtle.done()
---
+++ pybook/ch03/MathFunctions.py
@@ -0,0 +1,14 @@
+import math  # import math module to use the math functions
+
+# Test algebraic function
+print("exp(1.0) =", math.exp(1.0))
+print("log(2.78) =", math.log(math.e))
+print("log10(10, 10) =", math.log(10, 10))
+print("sqrt(4.0)=", math.sqrt(4.0))
+
+# Test trigonometric functions
+print("sin(PI / 2) =", math.sin(math.pi / 2))
+print("cos(PI / 2) =", math.cos(math.pi / 2))
+print("tan(PI / 2) =", math.tan(math.pi / 2))
+print("degrees(PI / 2) =", math.degrees(math.pi / 2))
+print("radians(PI / 2) =", math.radians(math.pi / 2))
