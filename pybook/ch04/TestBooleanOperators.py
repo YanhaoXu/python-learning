b497ec716909c705231b8d1f2b9739a5eb1ad6b3
yanhao.xu <yanhao.xu@outlook.com>
2021-11-22 20:19:14 +0800

    feature(ch04): chapter 4 selections
    source add

---
+++ pybook/ch04/TestBooleanOperators.py
@@ -0,0 +1,12 @@
+# Receive an input
+num = eval(input("Enter an integer: "))
+
+if num % 2 == 0 and num % 3 == 0:
+    print(num, "is divisible by 2 and 3")
+
+if num % 2 == 0 or num % 3 == 0:
+    print(num, "is divisible by 2 or 3")
+
+if (num % 2 == 0 or num % 3 == 0) and \
+        not (num % 2 == 0 and num % 3 == 0):
+    print(num, "is divisible by 2 or 3, but not both")
